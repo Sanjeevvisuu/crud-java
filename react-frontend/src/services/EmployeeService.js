dfed299e1935d02df2cfd1bd207ae48766c36db5
sanjeev <sanjeevvisuu@gamil.com>
2024-12-01 11:20:48 +0000

    aproject scuess fully running

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const EMPLOYEE_API_BASE_URL = "http://4.228.64.164:8080/api/v1/employees";
-const EMPLOYEE_API_BASE_URL = "http://4.228.64.164:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://47.129.219.58:8080/api/v1/employees";
 class EmployeeService {
 
     getEmployees(){
